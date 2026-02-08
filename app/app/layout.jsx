a34fa7bd9eb748f784c03487c6467e178e67f202
kxngtev-ctrl <kxngtev@gmail.com>
2026-02-08 10:50:34 -0500

    Create layout.jsx

---
+++ app/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Jeff Fullyauto | Official EPK",
+  description: "Official Electronic Press Kit for Jamaican dancehall artist Jeff Fullyauto",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
